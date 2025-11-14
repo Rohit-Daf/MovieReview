b57486af9900184f2f9273180566df49c4b867bc
Om Ingle <omingle7875@gmail.com>
2025-11-14 14:09:30 +0530

    Restore deleted pages (recover from 8d280fb)

---
+++ client/client/Pages/MyReviews/MyReviews.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar'
+
+function MyReviews() {
+  return (
+    <div>
+      <Navbar />
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">My Reviews</h1>
+        {/* Add reviews content here */}
+      </div>
+    </div>
+  )
+}
+
+export default MyReviews
