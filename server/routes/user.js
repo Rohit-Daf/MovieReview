198ffeae513592d1f2323012449ca422e62b9411
Rohit-Daf <rohitdaph2@gmail.com>
2025-11-14 09:44:16 +0530

    table and route login, register is created

---
+++ server/routes/user.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const pool = require("../db/db");
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+  const { first_name, last_name, email, password, mobile, birth } = req.body;
+  sql = `insert into users (first_name,last_name,email,password,mobile,birth) values(?,?,?,?,?,?)`;
+  pool.query(
+    sql,
+    [first_name, last_name, email, password, mobile, birth],
+    (error, data) => {
+      if (data) {
+        res.send({ status: "success", data: data });
+      } else {
+        res.send({ status: "error", error: "error" });
+      }
+    }
+  );
+});
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const sql = `select password from users where email=?`;
+  pool.query(sql, [email], (error, data) => {
+    if (data != "") {
+      const pass = data[0];
+      if (pass.password == password) {
+        res.send({ status: "success", data: data });
+      } else {
+        res.send({ status: "error", error: "Enter valid password" });
+      }
+    } else {
+      res.send({ status: "error", error: "Enter valid Email" });
+    }
+  });
+});
+
+router.get("/:acc_no", (req, res) => {
+  const { acc_no } = req.params;
+  const sql = `select acc_no,name,email,password,phone,date,typeofAccount from Ebank_TB where acc_no=?`;
+  pool.query(sql, [acc_no], (error, data) => {
+    if (data != "") {
+      const user = data[0];
+      res.send({ status: "success", data: user });
+    } else {
+      res.send({ status: "error", error: error });
+    }
+  });
+});
+
+module.exports = router;
